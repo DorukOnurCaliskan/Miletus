e59febe664a95008a8fd6453503de73b294f5d73
Doruk Onur Caliskan <63496036+dorukcaliskan02@users.noreply.github.com>
2023-07-24 14:15:04 +0300

    to dict and from dict of product order and restaurant

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -73,13 +73,29 @@ class User(db.Model, UserMixin):
         if is_new:
             setattr(self, 'password', data['password'])
 
+
 class Order(db.Model):
     __tablename__ = 'Order'
     order_id = db.Column(db.Integer, primary_key=True)
     order_address = db.Column(db.String(40), nullable=False)
-    #order_status
-    rest_id = db.Column(db.String(40), nullable=False)
+    order_status = db.Column(db.String(40), nullable=False)
+    rest_id = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+
+    def to_dict(self):
+        data = {
+            'order_id': self.order_id,
+            'order_address': self.order_address,
+            'order_status': self.order_status,
+            'rest_id': self.rest_id,
+            'user_id': self.user_id,
+        }
+        return data
+
+    def from_dict_alternative(self, data):
+        for field in ['order_address', 'order_status']:
+            if field in data:
+                setattr(self, field, data[field])
 
 
 class Restaurant(db.Model):
@@ -87,24 +103,58 @@ class Restaurant(db.Model):
     restaurant_id = db.Column(db.Integer, primary_key=True)
     restaurant_name = db.Column(db.String(40), nullable=False)
     restaurant_address = db.Column(db.String(40), nullable=False, unique=True)
-    #oepning_hour
-    #closing hour
-    #status
-
+    restaurant_type = db.Column(db.String(40), nullable=False)
+    restaurant_opening_hour = db.Column(db.Integer, nullable=False)
+    restaurant_closing_hour = db.Column(db.Integer, nullable=False)
+    restaurant_status = db.Column(db.String(40), nullable=False)
     products = db.relationship('Product', backref='Product')
+
+    def to_dict(self):
+        data = {
+            'restaurant_id': self.restaurant_id,
+            'restaurant_name': self.restaurant_name,
+            'restaurant_address': self.restaurant_address,
+            'restaurant_type': self.restaurant_type,
+            'restaurant_opening_hour': self.restaurant_opening_hour,
+            'restaurant_closing_hour': self.restaurant_closing_hour,
+            'restaurant_status': self.restaurant_status,
+        }
+        return data
+
+    def from_dict_alternative(self, data):
+        for field in ['restaurant_name', 'restaurant_address', 'restaurant_opening_hour', 'restaurant_closing_hour',
+                      'restaurant_type', 'restaurant_status']:
+            if field in data:
+                setattr(self, field, data[field])
 
 
 class Product(db.Model):
     __tablename__ = 'Product'
     product_id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.String(40), nullable=False)
+    product_name = db.Column(db.String(40), nullable=False, unique=True)
     product_price = db.Column(db.Integer, nullable=False)
     discount_amount = db.Column(db.Integer, nullable=False)
-    #status
-    #stock
+    product_status = db.Column(db.String(40), nullable=False)
+    product_stock = db.Column(db.Integer, nullable=False)  # stock
     restaurant_id = db.Column(db.Integer, db.ForeignKey('Restaurant.restaurant_id'))
 
+    def to_dict(self):
+        data = {
+            'product_id': self.product_id,
+            'product_name': self.product_name,
+            'product_price': self.product_price,
+            'discount_amount': self.discount_amount,
+            'product_status': self.product_status,
+            'product_stock': self.product_stock,
+            'restaurant_id': self.restaurant_id,
+        }
+        return data
 
+    def from_dict_alternative(self, data):
+        for field in ['product_name', 'product_price', 'discount_amount',
+                      'product_status', 'product_stock']:
+            if field in data:
+                setattr(self, field, data[field])
 
 # class Order(db.Model):
 #     pass
